47840ca1c05e92ff8dae1d30e1ded66a6f3fe949
Daniel <wazwazman1@gmail.com>
2025-09-29 14:02:36 +0300

    add minimal working provider

--- src/codelens/provider/CodeLensProvider.ts
+++ src/codelens/provider/CodeLensProvider.ts
@@ -1,11 +1,88 @@
 import * as vscode from 'vscode';
+import { AsyncCommandQueue } from './AsyncCommandQueue';
 
-class CodeLensProvider implements vscode.CodeLensProvider {
+type executeDocumentSymbolProviderResponse = vscode.SymbolInformation & vscode.DocumentSymbol;
+
+class ReferencesCodeLens extends vscode.CodeLens {
+    constructor(
+        public uri: vscode.Uri,
+        range: vscode.Range
+    ) {
+        super(range);
+    }
+}
+
+
+
+export class CodeLensProvider implements vscode.CodeLensProvider<ReferencesCodeLens> {
+    private queue: AsyncCommandQueue;
+
     constructor() {
+        this.queue = new AsyncCommandQueue();
     }
-    public async provideCodeLenses(document: vscode.TextDocument, _token: vscode.CancellationToken) {
+    public async provideCodeLenses(document: vscode.TextDocument, _token: vscode.CancellationToken): Promise<ReferencesCodeLens[]> {
+
+        const codeLenses: ReferencesCodeLens[] = [];
+
+        const topSymbols = await vscode.commands.executeCommand<executeDocumentSymbolProviderResponse[]>(
+            'vscode.executeDocumentSymbolProvider',
+            document.uri,
+        );
+
+        const allSymbols: (executeDocumentSymbolProviderResponse | vscode.DocumentSymbol)[] = [];
+
+        for (const symbol of topSymbols) {
+            allSymbols.push(symbol);
+            allSymbols.push(...(this.getChildSymbols(symbol.children)));
+        }
+
+        for (const symbol of allSymbols) {
+            codeLenses.push(new ReferencesCodeLens(document.uri, symbol.selectionRange));
+        }
+
+
+        return codeLenses;
     }
 
-    public async resolveCodeLens(codeLens: vscode.CodeLensProvider, token: vscode.CancellationToken) {
+    public async resolveCodeLens(codeLens: ReferencesCodeLens, token: vscode.CancellationToken): Promise<ReferencesCodeLens | null> {
+        if (vscode.workspace.getConfiguration("cpp-codelens").get("enableCodeLens", true)) {
+            return new Promise((resolve) => {
+                this.queue.enqueue(async () => {
+                    const document = await vscode.workspace.openTextDocument(codeLens.uri);
+                    const position = codeLens.range.start;
+
+                    // TODO: remove calle reference
+                    const refs: vscode.Location[] = await vscode.commands.executeCommand<vscode.Location[]>(
+                        "vscode.executeReferenceProvider",
+                        codeLens.uri,
+                        position);
+
+                    codeLens.command = {
+                        title: `${refs.length} references, ${document.getText(codeLens.range)}`,
+                        tooltip: "Tooltip provided by sample extension",
+                        command: "editor.action.peekLocations",
+                        arguments: [codeLens.uri, codeLens.range.start, refs]
+                    };
+                    resolve(codeLens);
+
+                });
+
+            });
+        }
+
+        return null;
+    }
+
+    private getChildSymbols(symbols: vscode.DocumentSymbol[]): vscode.DocumentSymbol[] {
+        const allSymbols: vscode.DocumentSymbol[] = [];
+
+        for (const symbol of symbols) {
+            allSymbols.push(symbol);
+            if (symbol.children) {
+                allSymbols.concat(this.getChildSymbols(symbol.children));
+            }
+        }
+
+        return allSymbols;
     }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,10 @@
 import * as vscode from 'vscode';
+import { CodeLensProvider } from './codelens/provider/CodeLensProvider';
 
 export function activate(context: vscode.ExtensionContext) {
+    const codelensProvider = new CodeLensProvider();
+    vscode.languages.registerCodeLensProvider("*", codelensProvider);
+
 }
 
 export function deactivate() { }
