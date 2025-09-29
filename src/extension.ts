866635dcb770deb5e315df0fa67210e45a0f952b
Daniel <wazwazman1@gmail.com>
2025-09-29 23:12:51 +0300

    add configChangeListener

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,29 @@
 import * as vscode from 'vscode';
 import { CodeLensProviderBuilder } from './codelens/CodeLensProviderBuilder';
+import { ConfigurationLoader } from './configuration/ConfigurationLoader';
 
 export function activate(context: vscode.ExtensionContext) {
-    
-    const codelensProvider =    CodeLensProviderBuilder.getCodeLens();
-    vscode.languages.registerCodeLensProvider("*", codelensProvider);
+
+    const configurationLoader = new ConfigurationLoader();
+    const codelensProvider = CodeLensProviderBuilder.getCodeLens();
+    let codeLensProviderDisposable = vscode.languages.registerCodeLensProvider("*", codelensProvider);
+
+    const configChangeListener = vscode.workspace.onDidChangeConfiguration((event: vscode.ConfigurationChangeEvent) => {
+        if (!event.affectsConfiguration('cpp-codelens')) {
+            return;
+        }
+
+        configurationLoader.updateConfiguration();
+        codeLensProviderDisposable.dispose();
+
+        const codelensProvider = CodeLensProviderBuilder.getCodeLens();
+        codeLensProviderDisposable = vscode.languages.registerCodeLensProvider("*", codelensProvider);
+
+        context.subscriptions.push(codeLensProviderDisposable);
+    });
+
+    context.subscriptions.push(codeLensProviderDisposable, configChangeListener);
+
 }
 
 export function deactivate() { }
